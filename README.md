57055606997404f2592bf914485ad6dda3bd18e1
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2015-10-02 19:06:14 -0400

    Reformatted output of bamSequenceInfo.pl to be useful for human or computer consumption; Added documentation to README for bamSequenceInfo.pl; added usage and argument checking to bamSequenceInfo.pl

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # bam-statistics
 Scripts that generate statistics for BAM files
+
+
+## bamSequenceInfo
+Reads information from a BAM file and generates a TSV with summary statistics about the following:
+
+* Read lengths, calculated with samtools
+* Normalized coverage, calculated by Picard's GcBiasMetrics
+* Base quality distributions using FastQC
+
+The full output from GcBiasMetrics and FastQC is generated and preserved for convenience.
+
+*Dependencies*
+
+* samtools
+* R
+* Picard tools 1.130+
+* fastqc
+* Reference fasta
+
+*Usage:*
+
+    perl bamSequenceInfo.pl --bam <path to bam file> --ref <path to reference file> [--help for more options] 
+    
+    Defaults are in [square brackets]
+        ref     path to genome reference fasta
+        bam     path to BAM file
+        samtools        path to samtools executable. [samtools]
+        picard  path to picard 1.30+ JAR file. [picard.jar]
+        fastqc  path to fastqc executable [fastqc]
+        r-path  path to R executable [R]
+        help    print this message
+
+*Output:*
+
+* (BAM)-GcBiasMetrics-chart.pdf - chart from Picard's CollectGcBiasMetrics
+* (BAM)-GcBiasMetrics-output.txt - full output from Picard's CollectGcBiasMetrics
+* (BAM).tsv - tsv with summary statistics of read lengths, normalized coverage, and base quality distribution
+* (BAM)_fastq.zip - results from FastQC
+
+Example output file:
+
+    	Min     Q1      Median  Mean    Q3      Max     StdDev
+    Read Length     	101     101     101     101     101     101     0
+    Normalized Coverage     17.00   21.00   21.00   20.96   21.00   23.00   0.6468507
+    Base Quality Distribution       25.75   25.89   26.71   26.91   27.71   28.94   1.096683
+
+
--- bamSequenceInfo.pl
+++ bamSequenceInfo.pl
@@ -3,20 +3,89 @@
 # This is a script to pull some stats from a BAM file in hopes to find which sequencing technology created it
 use strict;
 use warnings;
+use Getopt::Long;
 use Cwd 'abs_path';
 use File::Basename;
 
 # Set up location of BAM file
-my $BAMFile = $ARGV[0]; # File path of BAM file
-chomp($BAMFile);
-my $BAMFileName = fileparse($BAMFile, ".bam");
+my $BAMFile;    # File path of BAM file
+my $ref;        # File path of reference
+my $help;
+my $samtools = "samtools";
+my $picard   = "picard.jar";
+my $fastqc   = "fastqc";
+my $rpath    = "R";
+my $OutputFile;
 
-my $Technology = $ARGV[1]; # Type of technology
-my $ReadLengthFile = "$BAMFileName-Read-Length.txt";
+sub usage {
+    return
+"Usage: perl bamSequenceInfo.pl --bam <path to bam file> --ref <path to reference file> [--help for more options] \n\n";
+}
+
+sub options {
+    print "Defaults are in [square brackets]\n";
+    print "\tref	path to genome reference fasta\n";
+    print "\tbam	path to BAM file\n";
+    print "\tsamtools	path to samtools executable. [samtools]\n";
+    print "\tpicard	path to picard 1.30+ JAR file. [picard.jar]\n";
+    print "\tfastqc	path to fastqc executable [fastqc]\n";
+    print "\tr-path	path to R executable [R]\n";
+    print "\toutput	name or path of output file\n";
+    print "\thelp	print this message\n";
+}
+
+my $result = GetOptions(
+    "bam=s"      => \$BAMFile,
+    "ref=s"      => \$ref,
+    "samtools=s" => \$samtools,
+    "picard=s"   => \$picard,
+    "fastqc=s"   => \$fastqc,
+    "r-path=s"   => \$rpath,
+    "output=s"   => \$OutputFile,
+    "help"       => \$help
+) or die( "Error in command line arguments\n", usage() );
+
+if ( defined $help ) {
+    print usage();
+    options();
+    exit;
+}
+
+if ( not defined $BAMFile ) {
+    print "BAM file must be specified\n";
+    die(usage);
+}
+if ( not defined $ref ) {
+    print "Reference file must be specified\n";
+    die usage();
+}
+
+my $exitcode = system("$samtools -h >/dev/null 2>&1");
+if ( $exitcode == 127 ) {
+    die("samtools is not found at '$samtools'");
+}
+$exitcode = system("$fastqc -h >/dev/null 2>&1");
+if ( $exitcode == 127 ) {
+    die("fastqc is not found at '$fastqc'");
+}
+$exitcode = system("$rpath -h >/dev/null 2>&1");
+if ( $exitcode == 127 ) {
+    die("R is not found at '$rpath'");
+}
+
+###########################################################
+
+chomp($BAMFile);
+my $BAMFileName = fileparse( $BAMFile, ".bam" );
+
+#my $Technology      = $ARGV[1];                             # Type of technology
+my $ReadLengthFile  = "$BAMFileName-Read-Length.txt";
 my $ReadLengthStats = "$BAMFileName-Read-Length-Stats.txt";
 
 # Set up output file
-my $OutputFile = $BAMFileName . ".csv";
+if ( not defined $OutputFile ) {
+    $OutputFile = $BAMFileName . ".csv";
+}
 
 # Print basic script information
 print "*****bamSequenceInfo.pl*****\n";
@@ -28,33 +97,38 @@ print "\n";
 
 print "#####Generating statistics for read length\n";
 
-system(`samtools view $BAMFile | cut -f10 | awk '{print length}' | sort > $ReadLengthFile`);
+system(
+`$samtools view $BAMFile | cut -f10 | awk '{print length}' | sort > $ReadLengthFile`
+);
 
-#`R -q -e "x <- read.csv('$ReadLengthFile', header = F); sink('$ReadLengthStats', append=TRUE); summary(x); sd(x[ , 1]);d<-scan('$ReadLengthFile'); png('$ReadLengthGraph'); hist(d, col='#2196f3',xlab='Read Length (bp)', main='Distribution of Read Lengths'); sink('RComplete.file');"`;
 `R -q -e "x <- read.csv('$ReadLengthFile', header = F); sink('$ReadLengthStats', append=TRUE); summary(x);sd(x[ , 1]);sink('RComplete.file');"`;
 
 sleep 2 while not -e "RComplete.file";
 
 my $LineCount = 1;
 
-open my $READ_LENGTH_FH, "<", $ReadLengthStats or die "Cannot open '$ReadLengthStats'\n";
-open my $OUTPUT_FH, ">>", $OutputFile or die "Cannot open '$OutputFile'\n";
+open my $READ_LENGTH_FH, "<", $ReadLengthStats
+  or die "Cannot open '$ReadLengthStats'\n";
+open my $OUTPUT_FH, ">", $OutputFile or die "Cannot open '$OutputFile'\n";
+print $OUTPUT_FH "\tMin\tQ1\tMedian\tMean\tQ3\tMax\tStdDev\n";
+print $OUTPUT_FH "Read Length";
+while ( my $line = <$READ_LENGTH_FH> ) {
+    chomp($line);
+    $line =~ s/\s//g;
 
-while (my $line = <$READ_LENGTH_FH>) {
-	chomp($line);
-	$line =~ s/\s//g;
+    if ( $LineCount == 1 ) {
+        $LineCount += 1;
+        next;
+    }
+    elsif ( $LineCount == 8 ) {
+        $line =~ s/^\[.*\]//g;
+    }
+    else {
+        $line =~ s/^.*://g;
+    }
 
-	if ($LineCount == 1) {
-		$LineCount += 1;
-		next;
-	} elsif ($LineCount == 8) {
-		$line =~ s/^\[.*\]//g;
-	} else {
-		$line =~ s/^.*://g;
-	}
-	
-	print $OUTPUT_FH "$line,";
-	$LineCount += 1;
+    print $OUTPUT_FH "\t$line";
+    $LineCount += 1;
 }
 close $READ_LENGTH_FH;
 
@@ -65,91 +139,100 @@ print "#####Cleaning up files\n";
 
 # Generate statistics for coverage bias
 print "#####Generating statistics for coverage bias\n";
-`picard-tools CollectGcBiasMetrics CHART_OUTPUT=$BAMFileName-chart.pdf SUMMARY_OUTPUT=$BAMFileName-summary.txt WINDOW_SIZE=100 INPUT=$BAMFile OUTPUT=$BAMFileName-GC-Bias.txt REFERENCE_SEQUENCE=hg19_random.fa`;
+`java -jar $picard CollectGcBiasMetrics CHART_OUTPUT=$BAMFileName-GcBiasMetrics-chart.pdf SUMMARY_OUTPUT=$BAMFileName-GcBiasMetrics-summary.txt WINDOW_SIZE=100 INPUT=$BAMFile OUTPUT=$BAMFileName-GcBiasMetrics-output.txt REFERENCE_SEQUENCE=$ref`;
 
-`tail -n +8 $BAMFileName-GC-Bias.txt | cut -f5 > $BAMFileName-Normalized-Coverage.txt`;
+`tail -n +8 $BAMFileName-GcBiasMetrics-output.txt | cut -f5 > $BAMFileName-Normalized-Coverage.txt`;
 
 `R -q -e "x <- read.csv('$BAMFileName-Normalized-Coverage.txt', header = F); sink('$BAMFileName-Normalized-Coverage-stats.txt', append=TRUE); summary(x);sd(x[ , 1]);sink('RComplete2.file');"`;
 
 sleep 2 while not -e "RComplete2.file";
 
 $LineCount = 1;
-open my $COVERAGE_BIAS_FH, "<", "$BAMFileName-Normalized-Coverage-stats.txt" or die "Cannot open '$BAMFileName-Normalized-Coverage.txt'\n";
+open my $COVERAGE_BIAS_FH, "<", "$BAMFileName-Normalized-Coverage-stats.txt"
+  or die "Cannot open '$BAMFileName-Normalized-Coverage.txt'\n";
+print $OUTPUT_FH "\nNormalized Coverage";
+while ( my $line = <$COVERAGE_BIAS_FH> ) {
+    chomp($line);
+    $line =~ s/\s//g;
 
-while (my $line = <$COVERAGE_BIAS_FH>) {
-	chomp($line);
-        $line =~ s/\s//g;
-
-        if ($LineCount == 1) {
-                $LineCount += 1;
-                next;
-        } elsif ($LineCount == 8) {
-                $line =~ s/^\[.*\]//g;
-        } else {
-                $line =~ s/^.*://g;
-        }
-
-        print $OUTPUT_FH "$line,";
+    if ( $LineCount == 1 ) {
         $LineCount += 1;
+        next;
+    }
+    elsif ( $LineCount == 8 ) {
+        $line =~ s/^\[.*\]//g;
+    }
+    else {
+        $line =~ s/^.*://g;
+    }
+
+    print $OUTPUT_FH "\t$line";
+    $LineCount += 1;
 }
 
 close $COVERAGE_BIAS_FH;
 
 print "#####Cleaning up files\n";
-`rm $BAMFileName-summary.txt`;
+`rm $BAMFileName-GcBiasMetrics-summary.txt`;
 `rm $BAMFileName-Normalized-Coverage.txt`;
 `rm $BAMFileName-Normalized-Coverage-stats.txt`;
 `rm RComplete2.file`;
-`rm $BAMFileName-GC-Bias.txt`;
+
+#`rm $BAMFileName-GcBiasMetrics-output.txt`;
 
 # Generate Statistics for base quality score distribution
 print "#####Generating statistics for base quality score distribution...\n";
-`fastqc $BAMFile`;
+`$fastqc $BAMFile`;
 `unzip $BAMFileName"_fastqc.zip"`;
 
 my $FastQCOutputFile = $BAMFileName . "_fastqc/fastqc_data.txt";
-my $BaseQualityDist = "$BAMFileName-Base-Quality-Dist.txt";
+my $BaseQualityDist  = "$BAMFileName-Base-Quality-Dist.txt";
 
-open my $FASTQC_FH, '<', $FastQCOutputFile or die "Can't open '$FastQCOutputFile'\n";
-open my $BASE_QUALITY_FH, '>', $BaseQualityDist or die "Can't write to '$BaseQualityDist'\n";
+open my $FASTQC_FH, '<', $FastQCOutputFile
+  or die "Can't open '$FastQCOutputFile'\n";
+open my $BASE_QUALITY_FH, '>', $BaseQualityDist
+  or die "Can't write to '$BaseQualityDist'\n";
 
 while (<$FASTQC_FH>) {
-	chomp($_);
-        if (/>>Per base sequence quality/../>>END_MODULE/) {
-                next if />>Per base sequence quality/ || />>END_MODULE/;
-		my @Columns = split (/\t/, $_);
-		if ($Columns[1] ne "Mean"){
-                	print $BASE_QUALITY_FH "$Columns[1]\n";
-		}
+    chomp($_);
+    if ( />>Per base sequence quality/ .. />>END_MODULE/ ) {
+        next if />>Per base sequence quality/ || />>END_MODULE/;
+        my @Columns = split( /\t/, $_ );
+        if ( $Columns[1] ne "Mean" ) {
+            print $BASE_QUALITY_FH "$Columns[1]\n";
         }
+    }
 }
 close $BASE_QUALITY_FH;
 close $FASTQC_FH;
 
-`rm $BAMFileName-BaseQualityDist-stats.txt`;
+#`rm $BAMFileName-BaseQualityDist-stats.txt`;
 
 `R -q -e "x <- read.csv('$BaseQualityDist', header = F); sink('$BAMFileName-BaseQualityDist-stats.txt', append=TRUE); summary(x);sd(x[ , 1]);sink('RComplete3.file');"`;
 
 sleep 2 while not -e "RComplete3.file";
 
 $LineCount = 1;
-open my $BASE_QUALITY_STATS_FH, "<", "$BAMFileName-BaseQualityDist-stats.txt" or die "Cannot open '$BAMFileName-BaseQualityDist-stats.txt'\n";
+open my $BASE_QUALITY_STATS_FH, "<", "$BAMFileName-BaseQualityDist-stats.txt"
+  or die "Cannot open '$BAMFileName-BaseQualityDist-stats.txt'\n";
+print $OUTPUT_FH "\nBase Quality Distribution";
+while ( my $line = <$BASE_QUALITY_STATS_FH> ) {
+    chomp($line);
+    $line =~ s/\s//g;
 
-while (my $line = <$BASE_QUALITY_STATS_FH>) {
-       chomp($line);
-        $line =~ s/\s//g;
-
-        if ($LineCount == 1) {
-                $LineCount += 1;
-                next;
-        } elsif ($LineCount == 8) {
-                $line =~ s/^\[.*\]//g;
-        } else {
-                $line =~ s/^.*://g;
-        }
-
-        print $OUTPUT_FH "$line,";
+    if ( $LineCount == 1 ) {
         $LineCount += 1;
+        next;
+    }
+    elsif ( $LineCount == 8 ) {
+        $line =~ s/^\[.*\]//g;
+    }
+    else {
+        $line =~ s/^.*://g;
+    }
+
+    print $OUTPUT_FH "\t$line";
+    $LineCount += 1;
 }
 
 close $BASE_QUALITY_STATS_FH;
@@ -157,10 +240,13 @@ close $BASE_QUALITY_STATS_FH;
 print "#####Cleaning up files\n";
 `rm RComplete3.file`;
 `rm $BAMFileName-BaseQualityDist-stats.txt`;
-`rm $BAMFileName"_fastqc.zip"`;
-`rm -r $BAMFileBam"_fastqc"`;
-`rm $BAMFileName"_fastqc.html"`;
+
+#`rm $BAMFileName"_fastqc.zip"`;
+`rm -r $BAMFileName"_fastqc"`;
+
+#`rm $BAMFileName"_fastqc.html"`;
 `rm $BaseQualityDist`;
 
-print $OUTPUT_FH "$Technology\n";
+#print $OUTPUT_FH "$Technology\n";
+print $OUTPUT_FH "\n";
 close $OUTPUT_FH;
